34b5a48d0d52534634284d2df273b470601c30fb
In0rganicChem <6525min@gmail.com>
2026-03-22 13:10:26 +0900

    Update cleaning_assign.py

--- cleaning_assign.py
+++ cleaning_assign.py
@@ -228,7 +228,7 @@ first_week_exempt = st.multiselect(
     class_names,
     default=[]
 )
-st.markdown("")
+st.markdown(" ")
 seed_input = st.markdown("난수 시드를 정수로 입력하세요. 같은 난수 시드는 같은 결과를 보장합니다. 입력하지 않으면 랜덤으로 처리됩니다.", value="")
 seed_val = int(seed_input) if seed_input.isdigit() else None
 
